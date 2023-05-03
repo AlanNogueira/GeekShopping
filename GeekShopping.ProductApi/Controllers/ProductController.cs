252f1c5efdab46038209eaf6f54e1fddaaebf3c6
Alan Nogueira <alan.nogueira991@gmail.com>
2023-05-03 13:04:53 -0300

    COMMIT: Feat - Adicionadas funcionalidades de Create, Update e Delete de produtos.

--- GeekShopping.ProductApi/Controllers/ProductController.cs
+++ GeekShopping.ProductApi/Controllers/ProductController.cs
@@ -34,7 +34,7 @@ namespace GeekShopping.ProductApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
             if (vo is null) return BadRequest();
             var product = await _repository.Create(vo);
@@ -42,7 +42,7 @@ namespace GeekShopping.ProductApi.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
             if (vo is null) return BadRequest();
             var product = await _repository.Update(vo);
